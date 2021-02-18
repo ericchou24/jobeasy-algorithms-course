92696e3ab551d77736a2149a42142ef812855b07
ericchou24 <ericchou24@gmail.com>
2021-02-18 14:20:44 -0800

    HW1 of Algorithms course all in one Python file

---
+++ HW1_Algorithms.py
@@ -0,0 +1,29 @@
+##1 Sum of 3 modified
+def digit_sum(number):
+    sum_digits = 0
+    for digit in str(number):
+        sum_digits += int(digit)
+        return sum_digits
+
+##2 Find Max Number
+def find_max():
+    input_list = []
+    for i in range(3):
+        input_value = input("Please enter values. We will need 3 numbers: ")
+        input_list.append(input_value)
+    input_list.sort(reverse=True)
+    print(input_list[0])
+
+##3 Count Odd and even numbers.
+def odd_even_cut(number):
+    digits = []
+    even_count = 0
+    odd_count = 0
+    for digit in str(number):
+        digits.append(digit)
+    for i in digits:
+        if int(i) % 2 == 0:
+            even_count+=1
+        else:
+            odd_count+=1
+    print(f'Odd: {odd_count} Even: {even_count}')
